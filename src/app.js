4310bdc2639e05c95942ace9ae1e8a93d524c6ad
David Peralta Mendoza <davidmendoza911119@gmail.com>
2025-10-15 02:22:09 -0600

    feat: add logging config

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import logger from '#config/logger';
 
 const app = express();
 
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+// write logs inside logs folder
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
+
 app.get('/', (req, res) => {
-  res.send('Hello from Docker!').status(200);
+  logger.info('Hello world from logger!');
+  res.send('Hello world from the backend!').status(200);
 });
 
 export default app;
